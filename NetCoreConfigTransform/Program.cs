f64f2f4ed8682cb48aa93383c00dd9fd0b977a74
Al <alex17340@gmail.com>
2019-08-02 17:25:13 +0545

    first commit

---
+++ NetCoreConfigTransform/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+
+namespace NetCoreConfigTransform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value1 = ConfigurationManager.AppSettings["value1"];
+            Console.WriteLine($"Hello World! value1 is now {value1} ");
+        }
+
+
+    }
+}
